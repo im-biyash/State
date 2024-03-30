efa6970ce61b7b103d1b7e7b7007fc9d6490b9c8
biyash <bsstha21@gmail.com>
2024-03-30 18:13:02 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Statecolor from './Statecolor'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Statecolor />
+      </div>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/Statecolor.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import sushmita from "./assets/sushmita.png";
+
+
+
+function Statecolor() {
+  const [color, setColor] = useState(null);
+
+  const handleChange = (e) => {
+    const inputValue = e.target.value.toLowerCase(); // Convert input to lowercase for case-insensitive comparison
+    if (inputValue === "sushmita") {
+      setColor("white");
+    } else {
+      setColor(null);
+    }
+  };
+
+  return (
+    <div className="bg-black h-screen flex flex-col justify-center items-center">
+        <h1 className="text-white text-3xl">Enter your first name</h1>
+      <div
+        className="card bg-blue-200 h-[300px] w-[250px]"
+        style={{ backgroundColor: color }}
+      >
+        {color === "white" && (
+          <img src={sushmita} alt="Sushmita" className="w-full h-full object-cover" />
+        )}
+      </div>
+      <div className="absolute bottom-10">
+        <input
+          type="text"
+          className=" py-2 border border-gray-300 rounded-md mb-6"
+          onChange={handleChange}
+          placeholder="Enter first name"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Statecolor;
