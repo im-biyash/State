b4b1319d95e04bcc20c0ca41ce82adc7f1430a93
biyash <bsstha21@gmail.com>
2024-07-06 22:59:44 +0545

    change

--- src/Statecolor.jsx
+++ src/Statecolor.jsx
@@ -4,6 +4,8 @@ import bardan from "./assets/bardan.png";
 import aman from "./assets/aman.png"; 
 import sushma from "./assets/sushma.png";
 import biyash from "./assets/biyash.jpg";
+import binita from "./assets/binita.png"; // Import Binita's image
+
 function Statecolor() {
   const [color, setColor] = useState(null);
   const [imageName, setImageName] = useState(null);
@@ -16,23 +18,19 @@ function Statecolor() {
     } else if (inputValue === "bardan") {
       setColor("white");
       setImageName(bardan);
-    } 
-    else if(inputValue === 'lado'){
+    } else if (inputValue === 'lado') {
       setColor("white");
       setImageName(aman);
-    }
-    else if(inputValue === 'sushmaa' || inputValue === 'Sushma ' || inputValue === 'susma') {
+    } else if (inputValue === 'sushmaa' || inputValue === 'sushma' || inputValue === 'susma') {
       setColor("white");
-      setImageName(sushma)
-    }
-    else if(inputValue === 'biyash' ) {
+      setImageName(sushma);
+    } else if (inputValue === 'biyash') {
       setColor("black");
       setImageName(biyash);
-      // it change the state to biyash photo
-    }
-    
-    
-   else {
+    } else if (inputValue === 'binita') { // Add condition for Binita
+      setColor("pink");
+      setImageName(binita);
+    } else {
       setColor(null);
       setImageName(null);
     }
@@ -40,7 +38,7 @@ function Statecolor() {
 
   return (
     <div className="bg-black h-screen flex flex-col justify-center items-center">
-        <h1 className="text-white text-2xl mb-2">Enter your first name</h1>
+      <h1 className="text-white text-2xl mb-2">Enter your first name</h1>
       <div
         className="card bg-blue-400 h-[300px] w-[250px]"
         style={{ backgroundColor: color }}
@@ -52,7 +50,7 @@ function Statecolor() {
       <div className="absolute bottom-10">
         <input
           type="text"
-          className="py-2 relative top-[30px]  px-4 border border-gray-300 rounded-md mb-4 md:mb-0"
+          className="py-2 relative top-[30px] px-4 border border-gray-300 rounded-md mb-4 md:mb-0"
           onChange={handleChange}
           placeholder="Enter your first name"
         />
